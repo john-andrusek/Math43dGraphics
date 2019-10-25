d2c034f3991d2f499c5c37c06c385aaa477e3c36
andrusek <john.andrusek@ca.ibm.com>
2019-10-25 17:50:35 -0400

    initial checki

---
+++ assignment3/test.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public class test : ImmediateGeometry
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+  public override void _Process(float delta)
+  {
+/* 	  Clear();
+	  SetColor(new Color( 1, 1, 0, 1 ));
+Begin(Mesh.PrimitiveType.Lines);
+/*AddVertex(near1);
+AddVertex(near2);
+AddVertex(near2);
+AddVertex(near3);
+AddVertex(near3);
+AddVertex(near4);
+AddVertex(near4);
+AddVertex(near1); 
+ AddVertex(new Vector3(-1, -1, 0));
+ AddVertex(new Vector3(-1, 1, 0));
+ AddVertex(new Vector3(-1, 1, 0));
+ AddVertex(new Vector3(1, 1, 0));
+ AddVertex(new Vector3(1, 1, 0));
+ AddVertex(new Vector3(-1, -1, 0));
+ 
+ */
+
+   
+
+End();
+
+  }
+}
